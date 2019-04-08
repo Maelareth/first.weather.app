cd0bcc8e451b4e0ed1f316674eb7da851d836726
Maelareth <selinmartin@hotmail.com>
2019-04-08 19:29:15 +0200

    Applied first API-call

--- src/App.js
+++ src/App.js
@@ -6,26 +6,24 @@ import Forms from './components/Forms';
 import WeatherDay from './components/WeatherDay';
 
 
-const Api_Key= '802f6523acc961f46cc9a859bd6eae58'
+const Api_Key= '802f6523acc961f46cc9a859bd6eae58';
 
 
 class App extends React.Component {
-  getWeather = async () => {
-
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${Api_Key}`);
+  getWeather = async (e)=> {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Stockholm,se&appid=${Api_Key }&units=metric`);
 
     const data = await api_call.json();
     console.log(data);
-
-
-  }
+    }
 
   render () {
   return (
     <div>
-    <Titles />
-    <Forms />
-    <WeatherDay />          
+      <Titles />
+      <Forms getWeather={this.getWeather}/>
+      <WeatherDay />
     </div>
       );
     }
--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const Forms = (props) => {
+class Forms extends React.Component {
+  render(){
   return (
-      <form onSubmit={props.loadWeather}>
-        <input type="text" name="city" placeholder="City..." />
-        <input type="text" name="country" placeholder="Country..." />
-        <button>Get Weather</button>
+      <form onSubmit={this.props.getWeather}>
+          <input type="text" name="city" placeholder="City..." />
+          <input type="text" name="country" placeholder="Country..."/>
+          <button>Get Weather</button>
+
       </form>
-  )
-}
+    );
+  }
+};
 export default Forms;
