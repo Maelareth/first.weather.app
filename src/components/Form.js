c4c195e00a1f839a262afa762c3aba6c1146af3c
Maelareth <selinmartin@hotmail.com>
2019-04-11 11:34:41 +0200

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Form = props => (
 	<form onSubmit={props.getWeather}>
-		<input type="text" name="city" placeholder="City..."/>
-		<input type="text" name="country" placeholder="Country..."/>
-		<button>Get Weather</button>
+		<input type="text" name="city" placeholder="Stad..."/>
+		<input type="text" name="country" placeholder="Land..."/>
+		<button>Hitta väder</button>
 	</form>
 );
 
