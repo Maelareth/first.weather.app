2133f676796567b997c1931c6808997a6a93a2ec
Maelareth <selinmartin@hotmail.com>
2019-04-11 12:08:55 +0200

    Style commit2
    styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,22 +3,22 @@ import React from "react";
 const Weather = props => (
 	<div className="weather__info">
 	 {
-	 	props.city && props.country && <p className="weather__key"> Location:
+	 	props.city && props.country && <p className="weather__key"> Område:
 	 		<span className="weather__value"> { props.city }, { props.country }</span>
 	 	</p>
 	 }
 	 {
-	 	props.temperature && <p className="weather__key"> Temperature:
+	 	props.temperature && <p className="weather__key"> Temperatur:
 	 		<span className="weather__value"> { props.temperature }	°C </span>
 	 	</p>
 	 }
 	 {
-	 	props.humidity && <p className="weather__key"> Humidity:
+	 	props.humidity && <p className="weather__key"> Luftfuktighet:
 	 		<span className="weather__value"> { props.humidity } % </span>
 	 	</p>
 	 }
 	 {
-	 	props.description && <p className="weather__key"> Conditions:
+	 	props.description && <p className="weather__key"> Väder:
 	 		<span className="weather__value"> { props.description } </span>
 	 </p>
 	 }
